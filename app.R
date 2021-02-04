f84201ba54733c7e75467aaa73c2f65214e3dabe
MonyakeR <monyakerestelsiitsoe@gmail.com>
2021-02-04 12:17:58 +0200

    configurations added

--- app.R
+++ app.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(wordcloud2)
 library(quanteda)
 library(shiny)
+library(colourpicker)
 
 ui <- fluidPage(
   titlePanel("Free Word Cloud Generator"),
@@ -19,7 +20,70 @@ ui <- fluidPage(
              sidebarLayout(
                
                sidebarPanel(
-                 helpText("Enter text to generate wordcloud")
+                 
+                 helpText(h3("Use the options below to customise the word cloud")),
+                 
+                 selectInput(
+                   inputId = "font",
+                   label = "Font family",
+                   choices = list(
+                     "sans-serif",
+                     "monospace",
+                     "cursive",
+                     "Playfair display",
+                     "Open Sans",
+                     "Poppins",
+                     "Rubik",
+                     "Montserrat",
+                     "Oswald",
+                     "Quicksand",
+                     "fantasy",
+                     "system-ui",
+                     "ui-serif",
+                     "ui-sans-serif",
+                     "ui-monospace",
+                     "ui-rounded",
+                     "emoji",
+                     "math",
+                     "fangsong"
+                   ),
+                   selected = "sans-serif"
+                 ),
+                 
+                 selectInput(
+                   inputId = "shape",
+                   label = "Shape of the cloud to draw",
+                   choices = list(
+                     "circle",
+                     "cardioid",
+                     "diamond",
+                     "triangle-forward",
+                     "triangle",
+                     "pentagon",
+                     "star"
+                   ),
+                   selected = "circle"
+                 ),
+                 
+                 colourInput(
+                   inputId = "background",
+                   value = "white",
+                   label = "Colour of the background",
+                   returnName = TRUE,
+                   palette = "limited",
+                   closeOnClick = TRUE
+                 ),
+                 
+                 selectInput(
+                   inputId = "colour",
+                   label = "Colour of text",
+                   choices = list(
+                     "random-dark",
+                     "random-light"
+                   ),
+                   selected = "random-dark"
+                 )
+                 
                ),
                mainPanel(
                  wordcloud2Output("mywordcloud"))
@@ -38,7 +102,6 @@ server = function(input, output, session) {
     my_corpus <- corpus(data)
     
     # construct the document feature matrix
-    
     dfmat <- my_corpus %>% 
       tokens(remove_punct = TRUE,
              remove_symbols = TRUE,
@@ -55,11 +118,23 @@ server = function(input, output, session) {
     word_df <- as_tibble(term_frequency) %>% 
       rename(word = feature,freq = frequency) %>% 
       select(word, freq)
+    
     return(word_df)
+    
   })
   
+  
   output$mywordcloud <- renderWordcloud2({
-    wordcloud2(data_source())
+    
+    wordcloud2(
+      data_source(),
+      fontFamily = input$font,
+      backgroundColor = input$background,
+      color = input$colour,
+      shape = input$shape,
+      rotateRatio = 0
+    )
+    
   })
 }
 
